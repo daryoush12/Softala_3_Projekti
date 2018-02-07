687e7c299fba076da938c4d4a2bab157ba1c0bc4
Daryoush <daryoush_farsimadan@hotmail.com>
2018-02-07 13:54:02 +0200

    Configuration 1/2

--- React/kyselyreact/src/App.js
+++ React/kyselyreact/src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styles from 'css/styles';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Grid fluid>
+        <Row>
+          <Col xs={6} md={3}>
+            Hello, world!
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
